c1bcb60d4b3f2bfe77f956d3772da71227b74aff
Your Name <sheryarbahaduray11@gmail.com>
2023-10-11 15:44:40 +0500

    Testimonial-Box

---
+++ script.js
@@ -0,0 +1,6 @@
+const testimonialsContainer = document.querySelector('.testimonials-container')
+const testimonial = document.querySelector('.testimonial')
+const userImage = document.querySelector('.user-image')
+const username = document.querySelector('.username')
+const role = document.querySelector('.role')
+
